24c95c96dc25cf5780e2f036d7a76f4afe8bd7c9
cciia <agustinafransisca912@gmail.com>
2025-05-22 13:32:36 +0700

    berita

--- app/Models/Berita.php
+++ app/Models/Berita.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Berita extends Model
 {
-    //
+    use HasFactory;
+
+    // Nama tabel jika tidak mengikuti konvensi jamak
+    protected $table = 'beritas';
+
+    // Field yang bisa diisi secara massal
+    protected $fillable = [
+        'judul',
+        'isi',
+        'gambar',
+        'kategori',
+    ];
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BeritaController;
 
+
 Route::get('/', [HomeController::class, 'index']);
-// Resource controller untuk berita dan admin (sesuaikan kalau perlu)
+
 Route::resource('beritas', BeritaController::class);
 Route::resource('admin/berita', BeritaController::class);
 
