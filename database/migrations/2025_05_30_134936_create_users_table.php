e802b68e822c42d4cc3000cf5b3473ecf3a3144a
cciia <agustinafransisca912@gmail.com>
2025-06-03 18:22:04 +0700

    user seeder done

--- database/migrations/2025_05_30_134936_create_users_table.php
+++ database/migrations/2025_05_30_134936_create_users_table.php
@@ -12,8 +12,10 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable(); 
             $table->string('password');
-            $table->string('role')->default('user'); // kolom role untuk bedakan admin/user
+            $table->string('role')->default('user'); 
+            $table->rememberToken();
             $table->timestamps();
         });
     }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+          DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'admin',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
